788e9520a966ab9c32a0d2048e0d1afdd2c8ff9f
Dogy007 <lukas.neuenschwander@gmx.net>
2017-12-21 23:11:10 +0100

    Updated Install Script , added screen-switch script

--- install/00-prerequisite.sh
+++ install/00-prerequisite.sh
@@ -6,4 +6,26 @@ sudo mv 00-screenHandlerDisplayModeRunner.sh /etc/profile.d/00-screenHandlerDisp
 sudo mv 00-screenHandlerJcDriverRunner.sh /etc/profile.d/00-screenHandlerJcDriverRunner.sh
 sudo mv 00-screenHandlerPowerStateOnIndicatorRunner.sh /etc/profile.d/00-screenHandlerPowerStateOnIndicatorRunner.sh
 sudo mv 00-screenHandlerShutdownListenerRunner.sh /etc/profile.d/00-screenHandlerShutdownListenerRunner.sh
+#General configuration
+
+echo "gpu_mem=128
+audio_pwm_mode=2
+display_default_lcd=1" | sudo tee -a /boot/config.txt
+#Python
+
+sudo apt-get install -y python-setuptools python-dev
+sudo apt-get install -y python3 python3-pip
+sudo easy_install rpi.gpio
+
+cd..
+sudo mkdir git
+cd git
+git clone https://github.com/pimoroni/python-multitouch.git
+cd python-multitouch/library
+sudo python3 setup.py install
+sudo pip3 install python-uinput pyudev rpi_backlight
+#Install screen
+sudo apt-get install -y screen
+#Create screen-switch-scripts
+cd /home/pi/swipi-retro/assets/
 
