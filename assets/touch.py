9eff464e7041f46567894574dfc1b3d741b8256b
Dogy007 <lukas.neuenschwander@gmx.net>
2018-06-27 20:40:39 +0200

    Added Volumebar slider

--- assets/touch.py
+++ assets/touch.py
@@ -2,21 +2,34 @@
 # This file was derived from: https://github.com/drew-wallace/switchberry
 
 import struct
+import os
 import time
 import math
+import signal
 import glob
+import sys
 from ft5406 import Touchscreen, TS_PRESS, TS_RELEASE, TS_MOVE
 from math import floor
 from subprocess import call
+from subprocess import check_output
 import subprocess as SP
 import rpi_backlight as bl
 screenState = "/home/pi/swipi-retro/assets/currentDisplayMode"
+PNGVIEWPATH = "/home/pi/Mintybatterymonitor/Pngview/"
+ICONPATH = "/home/pi/Mintybatterymonitor/icons"
+brightLast = ""
+layerList = []
 def read_and_emulate_mouse(event, touch):
     global startX
     global startY
     global startTime
     global shouldRun
-
+    global brightLast
+    global killid
+    global layerList
+    if event == TS_RELEASE:
+        os.system("sudo kill " + "$(ps aux | grep '[p]ngview' | awk '{print $2}')")
+        layerList = []
     if event == TS_PRESS:
         (startX, startY) = touch.position
         startTime = time.time()
@@ -28,7 +41,44 @@ def read_and_emulate_mouse(event, touch):
     # top left: brightness
     if startX < 244 and startY < 140 and x <= 244:
         bl.set_brightness(x + 11)
-        
+        brightnessValue = x + 11
+        brightnessPng = ""
+        if brightnessValue >= 5 and brightnessValue < 29:
+            brightnessPng = "10"
+        elif brightnessValue >= 30 and brightnessValue < 54:
+            brightnessPng = "20"
+        elif brightnessValue >= 55 and brightnessValue < 79:
+            brightnessPng = "30"
+        elif brightnessValue >= 80 and brightnessValue < 104:
+            brightnessPng = "40"
+        elif brightnessValue >= 105 and brightnessValue < 129:
+            brightnessPng = "50"
+        elif brightnessValue >= 130 and brightnessValue < 154:
+            brightnessPng = "60"
+        elif brightnessValue >= 155 and brightnessValue < 179:
+            brightnessPng = "70"
+        elif brightnessValue >= 180 and brightnessValue < 204:
+            brightnessPng = "80"        
+        elif brightnessValue >= 205 and brightnessValue < 230:
+            brightnessPng = "90"
+        elif brightnessValue >= 231:
+            brightnessPng = "100"
+        if brightLast != brightnessPng and brightnessPng != "":
+            brightLast = brightnessPng
+            if len(layerList) == 0:
+                os.system("/home/pi/swipi-retro/assets/Pngview" + "/pngview -b 0 -l 3000" + "1" + " -x 260 -y 150 " + "/home/pi/swipi-retro/assets/icons/brightness/" + "bar" + ".png &")			
+            os.system("/home/pi/swipi-retro/assets/Pngview" + "/pngview -b 0 -l 3000" + brightnessPng + " -x 260 -y 150 " + "/home/pi/swipi-retro/assets/icons/brightness/" + brightnessPng + ".png &")
+            killBrightnessID = check_output("ps aux | grep '[p]ngview' | awk '{print $2}'", shell=True)
+            killBrightnessID = killBrightnessID.decode("utf-8")
+            killBrightnessID = killBrightnessID.replace("\n", " ")
+            killBrightnessID = killBrightnessID.split(" ")
+            for layer in killBrightnessID:
+                if layer != "" and layer not in layerList:
+                    layerList.append(layer)
+            if len(layerList) == 3:
+                killThisID = str(layerList[1])
+                os.system("sudo kill " + killThisID)
+                layerList.remove(layerList[1])
     # bottom left: volume
     if startX < 244 and startY > 340 and x <= 244:
         call(["amixer", "cset", "numid=1", "--", str(floor(x/2.44)) + '%'])
